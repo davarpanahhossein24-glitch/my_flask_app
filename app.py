7be3f088b72cd049be202f62482f7d9da9e4ac28
unknown <hosein467600@gmail.com>
2025-08-22 16:55:37 +0330

    Initial commit

--- app.py
+++ app.py
@@ -525,7 +525,8 @@ with app.app_context():
 
 
 # ---------- اجرا ----------
-# if __name__ == '__main__':
-#     with app.app_context():
-#         db.create_all()
-#     app.run(debug=True)
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
+
